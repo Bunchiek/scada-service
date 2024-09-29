bdc8ad4f4461317b6cef16c3af14ea5c23f99485
Aleksey Chernov <alexidal@mail.ru>
2024-09-29 16:24:35 +0300

    added dynamic tags adding

--- src/main/java/com/example/scada_service/configuration/OpcUaConfig.java
+++ src/main/java/com/example/scada_service/configuration/OpcUaConfig.java
@@ -1,23 +1,30 @@
 package com.example.scada_service.configuration;
 
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 @Component
-@ConfigurationProperties(prefix = "opcua")
 public class OpcUaConfig {
-
     private List<String> tags;
+
+    @PostConstruct
+    public void loadTagsFromFile() {
+        try (InputStream inputStream = new ClassPathResource("tags.yml").getInputStream()) {
+            Yaml yaml = new Yaml();
+            Map<String, Object> data = yaml.load(inputStream);
+            tags = (List<String>) data.get("tags");
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при загрузке тегов из файла tags.yml", e);
+        }
+    }
 
     public List<String> getTags() {
         return tags;
     }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
 }
